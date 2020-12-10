7e5fd10d25bd9140b444aeee05c0e7b9a3a52537
DanTheMan114 <danielturner184@gmail.com>
2020-12-09 21:55:26 -0800

    Done.

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,13 @@
+require 'pry'
 class Application
 
-    
+
     def call(env)
         resp = Rack::Response.new
         req = Rack::Request.new(env)
 
         if req.path.match(/items/)
+            binidng.pry
             item_name = req.path.split("/items/").last
             if item = @@items.find{|i| i.name == item_name}
                 resp.write item.price
